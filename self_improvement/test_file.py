74c9b693bcca5c0126346b4022930bd245291584
sidereior <alexanderlnanda@gmail.com>
2023-12-13 00:57:23 -0500

    first commit

--- self_improvement/test_file.py
+++ self_improvement/test_file.py
@@ -1,3 +1,3 @@
 def is_palindrome(s: str) -> bool:
-    clean_s = ''.join((char for char in s if char.isalnum())).lower()
-    return clean_s == clean_s[::-1]
+    cleaned_str = ''.join((char for char in s if char.isalnum())).lower()
+    return cleaned_str == cleaned_str[::-1]

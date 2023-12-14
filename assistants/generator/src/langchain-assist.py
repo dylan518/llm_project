f3df19d521a1d2cc1488512d08c4f67d55c409b9
sidereior <alexanderlnanda@gmail.com>
2023-12-14 03:27:52 -0500

    langchain somewhat working

--- assistants/generator/src/langchain-assist.py
+++ assistants/generator/src/langchain-assist.py
@@ -1,23 +1,57 @@
 from langchain.agents.openai_assistant import OpenAIAssistantRunnable
 from langchain.agents import AgentExecutor
 from langchain.tools import DuckDuckGoSearchRun
+from langchain.tools import DynamicTool
+import langchain.tools as tools
 
-tools = [DuckDuckGoSearchRun()]
+# Function to create a new assistant
+def create_new_assistant(input_dict):
+    try:
+        new_assistant = OpenAIAssistantRunnable.create_assistant(
+            name=input_dict.get("name", "default_name"),
+            instructions=input_dict.get("instructions", "Default instructions."),
+            tools=tools,  # You can define a different set of tools if you want
+            model=input_dict.get("model", "gpt-4-1106-preview"),
+            as_agent=True
+        )
+        return f"Assistant '{input_dict.get('name', 'default_name')}' created successfully."
+    except Exception as e:
+        return str(e)
 
+# Define the AssistantCreator tool
+assistant_creator_tool = DynamicTool({
+    'name': 'AssistantCreator',
+    'description': 'Create a new assistant with given parameters.',
+    'func': create_new_assistant
+})
+
+# Define the initial set of tools, including the AssistantCreator
+tools = [
+    DuckDuckGoSearchRun(),
+    assistant_creator_tool
+]
+
+# Create the primary assistant
 assistant = OpenAIAssistantRunnable.create_assistant(
     name="langchain assistant",
-    instructions="You are an assistant. Help the user",
+    instructions="You are an assistant designed to help the user write, edit, improve, and run code.",
     tools=tools,
     model="gpt-4-1106-preview",
     as_agent=True,
 )
 
+# Create the AgentExecutor with the assistant and tools
 agent_executor = AgentExecutor(agent=assistant, tools=tools)
-output = agent_executor.invoke(
-    {
-        "content": 
-        "What's the weather in Columbus today divided by 2"
-    }
-)
 
-print(f"Content: {output['content']}\nOutput: {output['output']}")
+# Example invocation of the agent executor
+input_dict = {
+    "name": "new_assistant",
+    "instructions": "You are a new assistant with specific capabilities.",
+    "model": "gpt-4-1106-preview"
+}
+
+output = agent_executor.invoke({
+    "content": input_dict
+})
+
+print(f"Output: {output['output']}")
--- assistants/generator/src/test.py
+++ assistants/generator/src/test.py
@@ -4,64 +4,75 @@ import time
 # Initialize the client
 client = openai.OpenAI()
 
-# Define a function to create an assistant
+# Define a function to create an assistant and return its ID
 def create_assistant(name, instructions, tools, model):
-    return client.beta.assistants.create(
-        name=name,
-        instructions=instructions,
-        tools=tools,
-        model=model
-    )
+  # Send the request to OpenAI and return the ID
+  response = client.beta.assistants.create(
+    name=name,
+    instructions=instructions,
+    tools=tools,
+    model=model
+  )
+  return response.id
 
-
-# Step 1: Create an Assistant
-assistant = create_assistant(
-    name="Assistant Creator",
-    instructions="You can create other assistants. To do so, specify the name, instructions, tools, and model. The user will prompt you for a basic structure of other assistants you are to create and their behavior.",
-    tools=[{"type": "function", "function": {"name": "create_assistant"}}],
-    model="gpt-4-1106-preview"
+# Step 1: Create the Assistant Creator
+assistant_creator = create_assistant(
+  name="Assistant Creator",
+  instructions="I can help you create other assistants. Tell me the name, instructions, tools, and model for the new assistant, and I'll take care of it.",
+  tools=[{"type": "function", "function": {"name": "create_assistant"}}],
+  model="gpt-4-1106-preview"
 )
 
 # Step 2: Create a Thread
 thread = client.beta.threads.create()
 
-# Step 3: Add a Message to a Thread
-message = client.beta.threads.messages.create(
-    thread_id=thread.id,
-    role="user",
-    content="Can you create another assistant that is instructred to be a math helper?"
-)
-
-# Step 4: Run the Assistant
-run = client.beta.threads.runs.create(
-    thread_id=thread.id,
-    assistant_id=assistant.id,
-    instructions="Please address the user as Alex. The user has a premium account."
-)
-
-print(run.model_dump_json(indent=4))
+# Step 3: Initialize Interaction Loop
+user_message = ""  # Store user's message for processing
+new_assistant_requested = False  # Flag indicating user wants a new assistant
+new_assistant_info = {}  # Store information for creating the new assistant
 
 while True:
-    # Wait for 5 seconds
-    time.sleep(5)  
+  # Wait for 5 seconds
+  time.sleep(5)
 
-    # Retrieve the run status
-    run_status = client.beta.threads.runs.retrieve(
-        thread_id=thread.id,
-        run_id=run.id
-    )
-    print(run_status.model_dump_json(indent=4))
+  # Check for new messages in the thread
+  messages = client.beta.threads.messages.list(thread_id=thread.id)
 
-    # If run is completed, get messages
-    if run_status.status == 'completed':
-        messages = client.beta.threads.messages.list(
-            thread_id=thread.id
-        )
-        
-        # Loop through messages and print content based on role
-        for msg in messages.data:
-            role = msg.role
-            content = msg.content[0].text.value
-            print(f"{role.capitalize()}: {content}")
-        
-        break
+  # Process user message
+  for message in messages.data:
+    if message.role == "user":
+      user_message = message.content[0].text.value
+
+  # Check if user requested a new assistant
+  if "create assistant" in user_message.lower():
+    new_assistant_requested = True
+    user_message = ""  # Reset user message for further information
+
+  # Check if user provided information for the new assistant
+  if new_assistant_requested and any(key in user_message.lower() for key in ("name", "instructions", "tools", "model")):
+    # Extract relevant information from user's message
+    for key in ("name", "instructions", "tools", "model"):
+      if key in user_message.lower():
+        new_assistant_info[key] = user_message.split(key, 1)[1].strip()
+
+  # Create the new assistant if requested and information provided
+  if new_assistant_requested and new_assistant_info:
+    # Create the assistant and update instructions
+    new_assistant_id = create_assistant(**new_assistant_info)
+    assistant_creator_instructions = f"I can now create assistants like your requested {new_assistant_info['name']}. Ask me to create it with specific instructions."
+    client.beta.threads.messages.create(thread_id=thread.id, role="assistant", content=[assistant_creator_instructions])
+
+    # Start a new run with updated instructions and new assistant ID
+    client.beta.threads.runs.create(thread_id=thread.id, assistant_id=assistant_creator.id, instructions=assistant_creator_instructions)
+
+    # Clear information for the next request and reset flags
+    new_assistant_requested = False
+    new_assistant_info = {}
+
+  # Print messages based on roles
+  for message in messages.data:
+    print(f"{message.role.capitalize()}: {message.content[0].text.value}")
+
+# Close the thread when finished
+client.beta.threads.delete(thread_id=thread.id)
+

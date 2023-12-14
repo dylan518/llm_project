df606e6671983026d1025df870a68ad58fde2a2b
Dylan Wilson <dylanpwilson2005@gmail.com>
2023-12-14 10:34:20 -0800

    feedback on commit
    just some doc comments

--- assistants/generator/src/langchain-assist.py
+++ assistants/generator/src/langchain-assist.py
@@ -18,6 +18,36 @@ def create_new_assistant(input_dict):
     except Exception as e:
         return str(e)
 
+#where are you getting Dynamic tool from only saw this in some js langchain doc? I think this should be just from langchain,gets import Tool also you need to define the tool format its kind of a complicated json. Also what is the tool here you need to write python code to load the json into a function and run otherwise the tool doesn't do anything
+"""
+
+I think this is what you want:
+
+from langchain.agents import AgentType, Tool, initialize_agent
+from langchain.chains import LLMMathChain
+from langchain.llms import OpenAI
+from langchain.utilities import SerpAPIWrapper
+
+search = SerpAPIWrapper()
+tools = [
+    Tool(
+        name="Search",
+        func=search.run,
+        description="useful for when you need to answer questions about current events",
+    ),
+    Tool(
+        name="Music Search",
+        func=lambda x: "'All I Want For Christmas Is You' by Mariah Carey.",  # Mock Function
+        description="A Music search engine. Use this more than the normal search if the question is about Music, like 'who is the singer of yesterday?' or 'what is the most popular song in 2022?'",
+    ),
+]
+
+agent = initialize_agent(
+    tools,
+    OpenAI(temperature=0),
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True,
+)"""
 # Define the AssistantCreator tool
 assistant_creator_tool = DynamicTool({
     'name': 'AssistantCreator',
@@ -32,6 +62,7 @@ tools = [
 ]
 
 # Create the primary assistant
+# if they all have the same instructions it makes them all identical so it destroys the point of having multiple assistants instead the instructions should be a parameter for the assistant creator tool
 assistant = OpenAIAssistantRunnable.create_assistant(
     name="langchain assistant",
     instructions="You are an assistant designed to help the user write, edit, improve, and run code.",
